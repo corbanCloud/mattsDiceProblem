e3066675be5d72d6e2f23197459ded1f4c1a0982
corbanCloud <corbanCloud@users.noreply.github.com>
2017-07-16 22:26:26 -0700

    Moved dice into 'dice' dictionary, refactored roll to take a dice by name and record it

--- main.py
+++ main.py
@@ -12,34 +12,52 @@ parser.add_argument('green', type=natural_number)
 parser.add_argument('red', type=natural_number)
 parser.add_argument('yellow', type=natural_number)
 
-yellow = {
-  1: 1,
-  2: 1,
-  3: 2,
-  4: 100,
-  5: 101,
-  6: 102
+dice = {
+    'yellow' : {
+      1: 1,
+      2: 1,
+      3: 2,
+      4: 100,
+      5: 101,
+      6: 102
+    },
+    'red' : {
+      1: 1,
+      2: 2,
+      3: 2,
+      4: 2,
+      5: 3,
+      6: 103
+    },
+    'green' : {
+      1: 1,
+      2: 1,
+      3: 100,
+      4: 100,
+      5: 101,
+      6: 101
+    },
+    'blue' : {
+      1: 0,
+      2: 1,
+      3: 2,
+      4: 2,
+      5: 101,
+      6: 102
+    }
 }
-red = {
-  1: 1,
-  2: 2,
-  3: 2,
-  4: 2,
-  5: 3,
-  6: 103
+
+rollvalues = {
+  'blue' : [],
+  'red' : [],
+  'green' : [],
+  'yellow' : [],
 }
 
-green = {
-  1: 1,
-  2: 1,
-  3: 100,
-  4: 100,
-  5: 101,
-  6: 101
-}
+def roll(name):
+  die = dice[name]
+  value = die[random.choice(list(die))]
+  rollvalues[name] = value
+  return value
 
-def roll(die):
-  return die[random.choice(list(die))]
-
-
-print(roll(green))
+print(roll('blue'))
